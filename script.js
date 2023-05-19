19bb5047446cc1436b6a3e7df8135f171530adf1
Tati100509 <tatianecolenascimento@gmail.com>
2023-05-19 15:02:25 -0300

    construindo um código assíncrono

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
-var consultaCEP = fetch('https://viacep.com.br/ws/01001003/json/').then(resposta => resposta.json())
-.then(r => {
-    if(r.erro) {
-        throw Error('Esse Cep não existe')
-    }else
-    console.log(r)})
-.catch(erro => console.log(erro));
-
-console.log(consultaCEP);
+async function buscaEndereco(){
+var consultaCEP = await fetch('https://viacep.com.br/ws/01001000/json/')
+var consultaCepConvertida = await consultaCEP.json();
+console.log(consultaCepConvertida);
+}
+buscaEndereco();
